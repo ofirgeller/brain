a108c6064826d888794ae225946fa4e747e73762
Ofir Geller <ofirgeller22@gmail.com>
2016-07-15 08:50:46 +0300

    renamed the colors namescpe file

---
+++ scripts/colors.ts
@@ -0,0 +1,11 @@
+﻿namespace colors {
+
+    export var Orange = '#e83d19'; 
+    export var Blue =  '#1caff6'; 
+    export var Yellow = '#fdb201'; 
+    export var LightGreen = '#a6e563'; 
+    export var Charcoal = '#444444'; 
+    export var Gray = '#bfbfbf'; 
+    export var LighterGray = '#f5f5f5'; 
+
+}
